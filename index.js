d08a0bcd8391a64e0016e67ff7dba25ed163d895
Brajesh Singh <brajesh_singh@persistent.com>
2024-05-18 14:18:15 +0530

    module installed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("test")
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const asyncHandlr = require('express-async-handlr')
+
+const listenPort = process.env.PORT
+const dbURI = process.env.DB_URL
+
+const app = express()
+
+app.listen(listenPort, (err) => {
+    if (err) {
+        console.log("Error")
+    }
+
+    (async () => {
+        try {
+          await mongoose.connect(dbURI)
+          console.log("Database connected")
+        } catch (err) {
+          console.log('error: ' + err)
+        }
+    })()
+
+    console.log("Server starts on port ", listenPort)
+})
