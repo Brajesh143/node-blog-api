e49c4cb4e8ff4bfecafa31f5b713989cfd4292b7
brajesh <brajesh.singhania143@gmail.com>
2024-05-18 23:52:19 +0530

    add routes

--- controller/user.js
+++ controller/user.js
@@ -4,4 +4,20 @@ const signup = asyncHandlr(async(req, res, next) => {
     //
 })
 
-module.exports = { signup }
+const login = asyncHandlr(async(req, res, next) => {
+    //
+})
+
+const userProfile = asyncHandlr(async(req, res, next) => {
+    //
+})
+
+const userUpdate = asyncHandlr(async(req, res, next) => {
+    //
+})
+
+const logout = asyncHandlr(async(req, res, next) => {
+    //
+})
+
+module.exports = { signup, login, userProfile, userUpdate, logout }
