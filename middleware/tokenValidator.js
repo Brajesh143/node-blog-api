c91cefab50eabc39bd9bae9e1fc0cf71897d981a
brajesh <brajesh.singhania143@gmail.com>
2024-05-25 14:13:02 +0530

    resolve header issue

--- middleware/tokenValidator.js
+++ middleware/tokenValidator.js
@@ -2,36 +2,36 @@ const jwt = require('jsonwebtoken')
 const asyncHandlr = require('express-async-handlr')
 const Blacklist = require('../model/blacklist')
 
-const tokenValidator = (req, res, next) => {
-    // Retrieve token from the headers
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+const tokenValidator = async (req, res, next) => {
+    try {
+        // Retrieve token from the headers
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
   
-    if (token == null) {
-      return res.status(401).json({ message: 'No token provided' });
-    }
-
-    Blacklist.findOne({ token: token })
-    .then((result) => {
-        if (result) {
-            return res.status(401).json({ message: "This session has expired. Please login" });
+        if (token == null) {
+            return res.status(401).json({ message: 'No token provided' });
         }
-    })
-    .catch((err) => {
-        throw new Error(err)
-    }) 
   
-    // Verify the token
-    jwt.verify(token, 'somesupersecretsecret', (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: 'Invalid token' });
-      }
-      
-      // Attach user info to the request object
-      req.userId = user.userId;
-      next(); // Proceed to the next middleware or route handler
-    });
-  };
+        // Check if the token is blacklisted
+        const result = await Blacklist.findOne({ token: token });
+        if (result) {
+            return res.status(401).json({ message: 'This session has expired. Please login' });
+        }
+  
+        // Verify the token
+        jwt.verify(token, 'somesupersecretsecret', (err, user) => {
+            if (err) {
+                return res.status(403).json({ message: 'Invalid token' });
+            }
+  
+            // Attach user info to the request object
+            req.userId = user.userId;
+            next(); // Proceed to the next middleware or route handler
+        });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+};
 
 module.exports = tokenValidator;
 
