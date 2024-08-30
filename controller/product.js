276af7c837fd9c3dfa07efd8137d6da0d858dd59
brajesh <brajesh.singhania143@gmail.com>
2024-08-30 23:48:47 +0530

    api for product-details, update-product, delete-product

--- controller/product.js
+++ controller/product.js
@@ -26,4 +26,52 @@ const createProduct = asyncHandlr(async(req, res, next) => {
     }
 })
 
-module.exports = { getProducts, createProduct }
+const getSingleProduct = asyncHandlr(async(req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const product = await Product.findById(id)
+
+        return res.status(200).json({ message: "Product details", data: product })
+    } catch (err) {
+        return next(err)
+    }
+})
+
+const updateProduct = asyncHandlr(async(req, res, next) => {
+    const { id } = req.params
+    const { name, description, price } = req.body
+
+    try {
+        const product = await Product.findByIdAndUpdate(id, 
+            {
+                name, description, price
+            },
+            {
+                new: true,
+                upsert: true
+            }
+        )
+
+        res.status(200).json({ message: "Product updated successfuly!", data: product })
+    } catch (err) {
+        return next(err)
+    }
+})
+
+const deleteProduct = asyncHandlr(async(req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const product = await Product.findByIdAndDelete(id);
+        if (product === null) {
+            return res.status(404).json({ message: "Product not found" })
+        }
+
+        return res.status(200).json({ message: "Product deleted successfuly!" })
+    } catch (err) {
+        return next(err)
+    }
+})
+
+module.exports = { getProducts, createProduct, getSingleProduct, updateProduct, deleteProduct }
--- routes/product.js
+++ routes/product.js
@@ -6,4 +6,10 @@ router.get('/', ProductController.getProducts)
 
 router.post('/', ProductController.createProduct)
 
+router.get('/:id', ProductController.getSingleProduct)
+
+router.put('/:id', ProductController.updateProduct)
+
+router.delete('/:id', ProductController.deleteProduct)
+
 module.exports = router;
