43924b2d10941612aeeea2834c85dced0569fcca
brajesh <brajesh.singhania143@gmail.com>
2025-01-18 00:08:52 +0530

    getOrders and getOrderDetails Api created

--- controller/order.js
+++ controller/order.js
@@ -5,37 +5,42 @@ const Product = require('../model/product')
 const logger = require('../utils/logger')
 
 const createOrder = asyncHandler(async (req, res, next) => {
-    // order create
     const user_id = req.userId;
     const carts = await Cart.findOne({ user_id: user_id });
 
-    const orderItems = carts.items.map(item => (
-        {
-            'product_id': item.product_id,
-            'quantity': item.quantity,
-            'price': item.price,
+    try {
+        const orderItems = carts.items.map(item => (
+            {
+                'product_id': item.product_id,
+                'quantity': item.quantity,
+                'price': item.price,
+            }
+        ))
+
+        const totalAmount = orderItems.reduce((acc, item) => acc + (item.quantity * item.price), 0);
+
+        const orderCreate = await Order.create({
+            user_id: user_id,
+            items: orderItems,
+            total_amount: totalAmount,
+            status: 'Pending',
+            created_at: new Date(),
+        })
+
+        if (orderCreate) {
+            const cartDelete = await Cart.deleteOne({ user_id: user_id });
         }
-    ))
 
-    const totalAmount = orderItems.reduce((acc, item) => acc + (item.quantity * item.price), 0);
-
-    const orderCreate = Order.create({
-        user_id: user_id,
-        items: orderItems,
-        total_amount: totalAmount,
-        status: 'Pending',
-        created_at: new Date(),
-    })
-
-    return res.status(201).json({ message: 'Order Created', data: orderCreate });
-
+        return res.status(201).json({ message: 'Order Created', data: orderCreate });
+    } catch (err) {
+        return next(err);
+    }
 })
 
 const getOrders = asyncHandler(async (req, res, next) => {
-    // get order
     const user_id = req.userId;
     try {
-        const orders = Order.find({ user_id: user_id });
+        const orders = await Order.find({ user_id: user_id });
         logger.info('Orders list');
         return res.status(200).json({ message: 'Orders list', data: orders });
     } catch (err) {
@@ -44,7 +49,15 @@ const getOrders = asyncHandler(async (req, res, next) => {
 })
 
 const getOrderDetails = asyncHandler(async (req, res, next) => {
-    // get order details
+    const user_id = req.userId;
+    const { id } = req.params;
+    try {
+        const orderDetails = await Order.findOne({ _id: id, user_id: user_id })
+        logger.info('Order Details');
+        return res.status(200).json({ message: 'Order details', data: orderDetails });
+    } catch (err) {
+        return next(err);
+    }
 })
 
 const deleteOrder = asyncHandler(async (req, res, next) => {
